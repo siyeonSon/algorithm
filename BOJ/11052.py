3a7fe770c8c7770f25119574b1de52fa24aca571
siyeonSon <siyeon0209@naver.com>
2023-01-17 15:48:12 +0900

    카드 구매하기

---
+++ BOJ/11052.py
@@ -0,0 +1,18 @@
+# 2023-01-16 17:09:35
+# https://www.acmicpc.net/problem/11052
+
+import math
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+p = list(map(int, input().split()))
+
+for i in range(n) :
+    cost = []
+    for j in range(math.ceil(i/2)) :
+        cost.append(p[j] + p[i-j-1])  # 1+a, 2+b, 3+c ...
+    cost.append(p[i])  # 0+z
+    p[i] = max(cost)
+
+print(p[n-1])
