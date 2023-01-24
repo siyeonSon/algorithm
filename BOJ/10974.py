be4313cdfa33204bf0ee073c96d3fbda9751c79d
siyeonSon <siyeon0209@naver.com>
2023-01-24 21:18:10 +0900

    모든 순열 - itertools.permutations()

--- BOJ/10974.py
+++ BOJ/10974.py
@@ -1,11 +1,19 @@
 # 2023-01-24 21:12:50
 # https://www.acmicpc.net/problem/10974
 
+import itertools
 import sys
 input = sys.stdin.readline
 
+# itertools.permutations() 사용
 n = int(input())
+nums = [i for i in range(1, n+1)]
+for per in itertools.permutations(nums, n) :
+    print(*per)
 
+
+# 백트래킹 사용
+n = int(input())
 s = []
 def dfs() :
     if len(s) == n :
@@ -17,5 +25,4 @@ def dfs() :
             s.append(i)
             dfs()
             s.pop()
-
 dfs()
