909636c4c6253b49fd12c8edb130c131328e7c12
siyeonSon <siyeon0209@naver.com>
2023-02-09 11:34:17 +0900

    평균 일일 대여 요금 구하기

---
+++ SQL/SELECT/151136.sql
@@ -0,0 +1,4 @@
+-- 2023-02-09 11:33:36
+-- https://school.programmers.co.kr/learn/courses/30/lessons/151136
+
+SELECT round(avg(daily_fee)) as AVERAGE_FEE from CAR_RENTAL_COMPANY_CAR where car_type = 'SUV'
