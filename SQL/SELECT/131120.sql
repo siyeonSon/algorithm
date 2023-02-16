99546160cb5208ff82a897419ee6470c0c05077a
siyeonSon <siyeon0209@naver.com>
2023-02-16 14:33:06 +0900

    3월에 태어난 여성 회원 목록 출력하기

--- SQL/SELECT/131120.sql
+++ SQL/SELECT/131120.sql
@@ -1,3 +1,7 @@
+-- 2023-02-16 14:32:23
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131120
+
+
 SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') AS DATE_OF_BIRTH FROM MEMBER_PROFILE 
 WHERE GENDER = 'W'
     AND MONTH(DATE_OF_BIRTH) = 3
