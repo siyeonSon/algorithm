94aec8d964a1e45dfc87042c76484b12b8df0707
siyeonSon <siyeon0209@naver.com>
2023-02-21 11:44:13 +0900

    미로 탐색

--- BOJ/2178.py
+++ BOJ/2178.py
@@ -13,21 +13,18 @@ n, m = map(int, input().split())
 maze = [list(map(int, input().strip())) for _ in range(n)]
 visited = [[False] * m for _ in range(n)]
 
-def bfs(x, y) :
-    queue = deque()
-    queue.append([x, y])
+queue = deque()
+queue.append([0, 0])
 
-    while queue:
-        x, y = queue.popleft()
-        if x == n-1 and y == m-1 :  # 끝점이 나오면 끝냄
-            print(maze[n-1][m-1])  # 마지막에 누적된 합을 print
-            break
-        for i in range(4) :
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m :      
-                if visited[nx][ny] == False and maze[nx][ny] == 1 :
-                    maze[nx][ny] = maze[x][y] + 1  # 이동할 자리에 현재 누적된 값 + 1
-                    visited[nx][ny] = True
-                    queue.append([nx, ny])
-
-bfs(0,0)
+while queue:
+    x, y = queue.popleft()
+    if x == n-1 and y == m-1 :  # 끝점이 나오면 끝냄
+        print(maze[n-1][m-1])  # 마지막에 누적된 합을 print
+        break
+    for i in range(4) :
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m :      
+            if visited[nx][ny] == False and maze[nx][ny] == 1 :
+                maze[nx][ny] = maze[x][y] + 1  # 이동할 자리에 현재 누적된 값 + 1
+                visited[nx][ny] = True
+                queue.append([nx, ny])
