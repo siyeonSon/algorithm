92195ddee89540effb89669bae9df5b7f19e9d37
siyeonSon <siyeon0209@naver.com>
2023-02-24 16:00:01 +0900

    한 줄로 서기

--- BOJ/1138.py
+++ BOJ/1138.py
@@ -22,3 +22,24 @@ for i in range(1, n) :
     find_index(i)
 
 print(*ans)
+
+
+# 2023-02-24 14:12:43
+# https://www.acmicpc.net/problem/1138_2
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+think = list(map(int, input().split()))
+ans = [0] * n
+
+for i in range(n) :
+    cnt = 0
+    for j in range(n) :
+        if ans[j] == 0 :  # 아직 방문하지 않은 노드
+            if cnt == think[i] :
+                ans[j] = i+1
+                break
+            cnt += 1
+print(*ans)
