b26b6835dfc51b2918575ff44cd748a37a90d78a
siyeonSon <siyeon0209@naver.com>
2023-02-24 15:56:57 +0900

    없어진 기록 찾기

---
+++ SQL/JOIN/59042.sql
@@ -0,0 +1,6 @@
+-- 2023-02-24 15:56:50
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59042
+
+SELECT ANIMAL_ID, NAME FROM ANIMAL_OUTS
+WHERE ANIMAL_ID NOT IN
+    (SELECT ANIMAL_ID FROM ANIMAL_INS)
