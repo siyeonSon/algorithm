421395fb5ae01d4e2fd28deb2d38dd1aa967515a
siyeonSon <siyeon0209@naver.com>
2023-02-24 22:14:48 +0900

    팰린드롬 만들기

--- BOJ/1254.py
+++ BOJ/1254.py
@@ -4,37 +4,9 @@
 import sys
 input = sys.stdin.readline
 
-# 홀수인 경우
-def case1(str) :
-    mid = len(str) // 2
-    for i in range(mid) :
-        if str[mid-i] != str[mid+i] :
-            return False
-    return True
-
-# 짝수인 경우
-def case0(str) :
-    mid = len(str) // 2
-    end = len(str) - 1
-    for i in range(mid) :
-        if str[i] != str[end-i] :
-            return False
-    return True
-
-# palindrome인 경우 True 반환
-def palindrome(str) :
-    if len(str) % 2 == 0 :
-        return case0(str)
-    else :
-        return case1(str)
-
 s = input().strip()
-ans = len(s)
-if palindrome(s) :
-    print(ans)
-else :
-    for i in range(len(s)+1) :
-        str = s + s[i::-1]
-        if palindrome(str) :
-            break
-    print(ans + i + 1)
+
+for i in range(len(s)) :
+    if s[i:] == s[i:][::-1] : # if 순방향 == 역방향
+        print(len(s)+i)
+        break
