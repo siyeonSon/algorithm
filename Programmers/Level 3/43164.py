2962e00a8aaddfbcf2b232f9566b69d10293ba3e
siyeonSon <siyeon0209@naver.com>
2023-02-28 21:07:08 +0900

    여행경로

--- Programmers/Level 3/43164.py
+++ Programmers/Level 3/43164.py
@@ -1,36 +1,29 @@
 # 2023-02-28 19:24:49
 # https://school.programmers.co.kr/learn/courses/30/lessons/43164
 
-'''
-틀린 이유
-ICN -> A, A -> B, ICN -> C, C -> ICN 의 경우
-나의 풀이: ICN -> A -> B
-정답: ICN -> C -> ICN -> A -> B
-
-나의 풀이로는 모든 티켓을 사용할 수 없다.
-해결: 도착지로부터 출발 티켓이 없는 티켓을 나중에 사용해야 한다.
-'''
-
-import sys
-sys.setrecursionlimit(10**6)
-answer = ['ICN']
+from collections import deque
+from collections import defaultdict
 
 def solution(tickets):
-    global answer
-    tickets.sort(key = lambda x: (x[0], x[1]))
-    used = [False] * len(tickets)
-    print(tickets)
+    answer = []
+    tickets.sort(key = lambda x:(x[1], x[0]))
     
-    # "ICN" 공항에서 출발
-    dfs('ICN', tickets, used)
+    dic = defaultdict(list)
     
-    return answer
-
-def dfs(city, tickets, used) :
-    global answer
-    for i in range(len(tickets)) :
-        if tickets[i][0] == city and not used[i] :
-            if tickets[i][1] : 
-                used[i] = True  # 티켓 사용
-                answer.append(tickets[i][1])  # 도착지 저장
-                dfs(tickets[i][1], tickets, used)  # 도착지를 출발지로 하는 티켓 탐색
+    # 딕셔너리를 사용하여 중복 제거
+    for [start, end] in tickets:
+        dic[start].append(end)
+    for k in dic.keys():
+        dic[k].sort(reverse = True)
+    
+    def dfs():
+        stack = ["ICN"]
+        while stack:
+            print(stack)
+            start = stack[-1]
+            if not dic[start]: # start에서 출발하는 항공편이 없는경우 바로 답에 넣기
+                answer.append(stack.pop())
+            else:
+                stack.append(dic[start].pop())
+    dfs()
+    return answer[::-1]
