abcbfc3ce8beef318d15439c528df0e10cda2d1d
siyeonSon <siyeon0209@naver.com>
2023-02-28 19:19:56 +0900

    무한 수열

--- BOJ/1351.py
+++ BOJ/1351.py
@@ -1,18 +1,18 @@
 # 2023-02-28 14:30:37
 # https://www.acmicpc.net/problem/1351
 
-from collections import defaultdict
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 ans = 0
 n, p, q = map(int, input().split())
-a = defaultdict(int)  # [1]*(n+1)을 하면 메모리 초과남 -> dictionary 사용
+a = dict()  # [1]*(n+1)을 하면 메모리 초과남 -> dictionary 사용
 a[0] = 1
 
 def dp(x) :
     global ans
-    if a[x] != 0 :  # x==0 인 경우 && a[x] 값을 전에 구한 경우 재귀를 돌지 않음 -> 시간 초과 감소
+    if x in a :  # x==0 인 경우 && a[x] 값을 전에 구한 경우 재귀를 돌지 않음 -> 시간 초과 감소
         return a[x]
     a[x] = dp(x//p) + dp(x//q)
     return a[x]
