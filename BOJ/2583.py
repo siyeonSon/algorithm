8e442a55d276aa1f9b93716f7ce45ffadbcb0e6a
siyeonSon <siyeon0209@naver.com>
2025-10-20 21:49:27 +0900

    영역 구하기

--- BOJ/2583.py
+++ BOJ/2583.py
@@ -1,41 +1,54 @@
-# 2023-11-23 22:44:54
+# 2025-10-20 21:49:12
 # https://www.acmicpc.net/problem/2583
 
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(10**6)
+from collections import deque
 
 m, n, k = map(int, input().split())
+empty = [[True] * m for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
 
-visited = list([False]*m for _ in range(n))
 for _ in range(k) :
     x1, y1, x2, y2 = map(int, input().split())
-    for i in range(x1, x2):
-        for j in range(y1, y2):
-            visited[i][j] = True
+    for i in range(x1, x2) :
+        for j in range(y1, y2) :
+            empty[i][j] = False
 
-dx = [-1, 0, 0, 1]
-dy = [0, -1, 1, 0]
-
-def dfs(x, y) :
-    global area_count
-    visited[x][y] = True
-    area_count += 1
+dx, dy = [-1, 0, 0, 1], [0, -1, 1, 0]
+def dfs(x, y, area):
     for i in range(4) :
-        nx, ny = x+dx[i], y+dy[i]
+        nx, ny = dx[i]+x, dy[i]+y
         if 0 <= nx < n and 0 <= ny < m :
-            if not visited[nx][ny] :
-                dfs(nx, ny)
+            if empty[nx][ny] and not visited[nx][ny] :
+                visited[nx][ny] = True
+                area = dfs(nx, ny, area+1)
 
-count = 0
-areas = []
+    return area
+
+def bfs(x, y, area) :
+    queue = deque()
+    queue.append((x,y))
+
+    while queue :
+        x, y = queue.popleft()
+        for i in range(4) :
+            nx, ny = dx[i]+x, dy[i]+y
+            if 0 <= nx < n and 0 <= ny < m :
+                if empty[nx][ny] and not visited[nx][ny]:
+                    visited[nx][ny] = True   # 큐에 넣기 전에 방문 체크를 해야 중복 방문을 방지할 수 있음
+                    queue.append((nx, ny))
+                    area += 1
+    return area
+
+answer = []
 for i in range(n) :
     for j in range(m) :
-        if not visited[i][j] :
-            count += 1
-            area_count = 0
-            dfs(i, j)
-            areas.append(area_count)
+        if empty[i][j] and not visited[i][j] :
+            visited[i][j] = True
+            answer.append(bfs(i, j, 1))
 
-print(count)
-print(*sorted(areas))
+answer.sort()
+print(len(answer))
+print(*answer)
